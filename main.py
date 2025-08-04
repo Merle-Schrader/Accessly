2fcb062e3210619ffd9f366025e23bf276a51415
wwmeynardie <wwmeynardie@gmail.com>
2025-08-04 16:29:32 -0400

    Reworked structure of main.py so it doesn't run everything

--- main.py
+++ main.py
@@ -17,15 +17,16 @@ def add_options(options, **kwargs):
             raise ValueError('Could not find "' + options + '" in the accessibility options. Please consider adding it!')
 
     # If there are multiple options:
-    if type(options) != list:
+    elif type(options) != list:
         raise TypeError('Accessibility options should be a string or a list of strings!')
-    for option in options:
-        if type(option) != str:
-            raise TypeError('Accessibility options should be a list of strings!')
-    for option in options:
-        try:
-            file = __import__('accessibilities.access_' + option)
-            file.apply(kwargs[option])
-        except:
-            raise ValueError('Could not find "' + option + '" in the accessibility options. Please consider adding it!')
+    elif type(options) == list:
+        for option in options:
+            if type(option) != str:
+                raise TypeError('Accessibility options should be a list of strings!')
+        for option in options:
+            try:
+                file = __import__('accessibilities.access_' + option)
+                file.apply(kwargs[option])
+            except:
+                raise ValueError('Could not find "' + option + '" in the accessibility options. Please consider adding it!')
 
