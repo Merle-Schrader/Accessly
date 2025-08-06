52c2cdf4876037ec0af0a6a0801995bb3d0234b3
Sydney Andersen <sydneymandersen@gmail.com>
2025-08-06 08:00:13 -0700

    fixed duplicates

--- __init__.py
+++
@@ -1,2 +0,0 @@
-# imp the key functions that users will interact with
-from .core import configure, register_feature
--- config.py
+++
@@ -1,5 +0,0 @@
-# store global settings and feature registry
-settings = {}  # user-defined settings like {"colorblind": "protanopia"}
-show_hooks = []
-
-registered_features = {}  # maps "feature_name" -> apply_function
