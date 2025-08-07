716b52139b41f78879855e1f88cc2f75609e70bb
Sydney Andersen <sydneymandersen@gmail.com>
2025-08-07 06:46:14 -0700

    add weight option for legiblefonts

--- accessly/accessibilities/legiblefont.py
+++ accessly/accessibilities/legiblefont.py
@@ -4,85 +4,51 @@ import matplotlib.font_manager as fm
 from pyfonts import load_google_font
 import os
 
-def apply(lf_options):
+def apply(params):
     """
-    Registers a font style accessibility hook that changes all plot
-    text options to a selected font, that may be useful for dyslexic
-    or low-vision users.
-
-    Args:
-        lf_options (dict): {
-            "font": str  (e.g., "Comic Sans MS")
-            "bold": bool (toggle bold)
-
-        }
+    Registers a font style accessibility hook that changes  plot
+    text options, that may be useful for dyslexic or low-vision users.
+    Parameters:
+        font (str): selected font (default: "Atkinson Hyperlegible")
+        weight (str): set font weight (default: bold)
     """
-    if not isinstance(lf_options, dict):
-        print("[LegibleFont] WARNING: Expected dict, got", type(lf_options))
-        return
 
-    lf_font = lf_options.get("font", [])
+    lf_font = params if isinstance(params, str) else params.get("font", "Atkinson Hyperlegible")
+    lf_weight = params.get("weight", "bold")
+    if lf_weight not in ["normal", "bold"]:
+        print("[LegibleFont] WARNING: Invalid text weight selected. Use normal or bold.")
+
+    # color = params.get("color", "black")
+    # fontsize = params.get("fontsize", 14)
+
     if isinstance(lf_font, list):
         lf_font = lf_font[0]
         print("[LegibleFont] WARNING: Expected single font. Only using first list entry")
 
-    if isinstance(lf_font,str):
-        available_fonts_ext = [os.path.splitext(os.path.basename(i)) for i in fm.findSystemFonts(fontpaths=None, fontext='ttf')]
-        available_fonts = [i[0] for i in available_fonts_ext]
+    available_fonts_ext = [os.path.splitext(os.path.basename(i)) for i in fm.findSystemFonts(fontpaths=None, fontext='ttf')]
+    available_fonts = [i[0] for i in available_fonts_ext]
 
-        if lf_font not in available_fonts:
-            try:
-                #maybe it's a google font?
-                g_font = load_google_font(lf_font)
-                fm.fontManager.addfont(g_font.get_file())
-            except:
-                #(it wasn't)
-                print("[LegibleFont] WARNING: Selected ",lf_font, " but this font is not available!")
+    if lf_font not in available_fonts:
+        try:
+            #maybe it's a google font?
+            g_font = load_google_font(lf_font, weight=lf_weight)
+            fm.fontManager.addfont(g_font.get_file())
+        except:
+            #(it wasn't)
+            print("[LegibleFont] WARNING: Selected ",lf_font, " but this font is not available!")
 
     def font_update_current_figure():
         fig = plt.gcf()
+        #use rcParams to set font of ALL text
+        plt.rcParams["font.weight"] = lf_weight
+        plt.rcParams["axes.labelweight"] = lf_weight
+
+        #set font last, in case there are issues
         plt.rcParams["font.family"] = "sans-serif"
-        plt.rcParams["font.sans-serif"] = lf_font #assumes sans-serif for now
-        print("[LegibleFont] Using font: ",lf_font)
+        plt.rcParams["font.sans-serif"] = lf_font
 
-
-
-        # for ax in fig.axes:
-
-        #     for x in ax.get_xlabel():
-        #         x
-        #         try:
-        #             x.get_font
-
-                    
-                # except Exception as e:
-                #     print(f"[LegibleFont] Label Font Change Failed: {e}")
-
-            # label, title,legend handle labels, x and y
-            # # Scatter
-            # for col in ax.collections:
-            #     try:
-            #         facecolors = col.get_facecolors()
-            #         if facecolors is not None and len(facecolors) > 0:
-            #             new_colors = []
-            #             for rgba in facecolors:
-            #                 rgb = rgba[:3]
-            #                 new_rgb = remap_rgb_to_safe(rgb, cb_types)
-            #                 new_colors.append(to_rgba(new_rgb, alpha=rgba[3]))
-            #             col.set_facecolors(new_colors)
-
-            #         edgecolors = col.get_edgecolors()
-            #         if edgecolors is not None and len(edgecolors) > 0:
-            #             new_edges = []
-            #             for rgba in edgecolors:
-            #                 rgb = rgba[:3]
-            #                 new_rgb = remap_rgb_to_safe(rgb, cb_types)
-            #                 new_edges.append(to_rgba(new_rgb, alpha=rgba[3]))
-            #             col.set_edgecolors(new_edges)
-
-            #     except Exception as e:
-            #         print(f"[Colorblind] Scatter recolor failed: {e}")
-
+        print("[LegibleFont] Using font:",lf_font)
+        print("[LegibleFont] Using weight:",lf_weight)
 
     config.show_hooks.append(font_update_current_figure)
 
